aacdea88abe232456661614d21fa0316254c903c
Tamilselvan K <tamils1978@gmail.com>
2025-01-01 12:37:48 +0530

    Customer Service
    Customer Service

--- src/app/modules/services/customer.service.ts
+++ src/app/modules/services/customer.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { AuthModel } from '../auth/models/auth.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,31 @@ import { Observable } from 'rxjs';
 export class CustomerService {
 
   private apiUrl = 'http://localhost:9200/stashook/searchCustomer';
-
+  private authLocalStorageToken = `${environment.appVersion}-${environment.USERDATA_KEY}`;
   constructor(private http: HttpClient) {}
 
   searchCustomer(search: string = ''): Observable<any[]> {
-      return this.http.post<any[]>(`${this.apiUrl}?search=${search}`,"page");
+    var auth = this.getAuthFromLocalStorage();
+       const httpHeaders = new HttpHeaders({
+      Authorization: `${auth?auth.authToken:""}`,
+    });
+      return this.http.post<any[]>(`${this.apiUrl}?search=${search}`,"page",{
+        headers: httpHeaders,
+      });
   }
 
+private getAuthFromLocalStorage(): AuthModel | undefined {
+  try {
+    const lsValue = localStorage.getItem(this.authLocalStorageToken);
+    if (!lsValue) {
+      return undefined;
+    }
+
+    const authData = JSON.parse(lsValue);
+    return authData;
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+}
 }
