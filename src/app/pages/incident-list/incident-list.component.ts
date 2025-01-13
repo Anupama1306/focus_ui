1233f8611105f1a650248384dc13dc34abc3c83d
muthuslm2006 <98971383+muthuslm2006@users.noreply.github.com>
2025-01-13 10:51:26 +0530

    init incident service

--- src/app/pages/incident-list/incident-list.component.ts
+++ src/app/pages/incident-list/incident-list.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { IncidentService } from './../../modules/services/incident.service';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from "../../_metronic/shared/shared.module";
+import { Incident } from 'src/app/modules/auth/models/incident.model';
+import { CommonModule } from '@angular/common';
+import { catchError, of, tap } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-incident-list',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule,CommonModule],
   templateUrl: './incident-list.component.html',
   styleUrl: './incident-list.component.scss'
 })
-export class IncidentListComponent {
+export class IncidentListComponent implements OnInit{
+  formGroup: FormGroup;
+
+      constructor(   public incidentService:IncidentService) {
+ this.formGroup = new FormGroup({
+        // customerId:
+        cookBookName:new FormControl(),
+        categoryName: new FormControl(),
+        mobileNumber: new FormControl(),
+        priority: new FormControl(),
+
+
+      })
+    }
+      allIncident: Incident[];
+
+      ngOnInit():void{
+        this.searchIncedent("");
+      }
+
+        searchIncedent(searchText:string){
+          this.incidentService.searchIncident(searchText).pipe(
+            tap((res) => {
+              console.log("Res",res);
+              this.allIncident= res.data;
+        }),
+            catchError((errorMessage) => {
+              console.log(errorMessage,"X");
+              return of();
+            }),
+          ).subscribe(res =>console.log(res,"!!"));
+
+        }
+
+
 
 }
