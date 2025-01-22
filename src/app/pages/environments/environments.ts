99d86e352896f3ed6259e83e9b790d12d41641f4
ananthee89 <ananthee89@gmail.com>
2025-01-22 09:35:31 +0530

    environment file
    environment file

--- src/app/pages/environments/environments.ts
+++ src/app/pages/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environmentpath={
-  searchCustomer :'http://localhost:9201/stashook/searchCustomer',
-  addCustomer :'http://localhost:9201/stashook/addCustomer',
+  searchCustomer :'http://localhost:9200/stashook/searchCustomer',
+  addCustomer :'http://localhost:9200/stashook/addCustomer',
   searchIncident : 'http://localhost:9200/stashook/searchIncident',
   apiUrl: 'http://localhost:9000',
   addIncident :'http://localhost:9200/stashook/createIncident',
